49ed7305991820b7ecd4992b628732ffe2aa60bc
sigitp-git <sigitp@amazon.com>
2020-05-10 19:49:25 -0500

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import{ Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,7 +8,10 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn&#39;t exist... the sadness....</p>
+    <p>You might have older link from my previous blog, now I migrated my blog to be completely serverless, please visit the new{` `} 
+    <Link to="/">https://sigit.cloud</Link>!! </p>
+    <br/> <br/>
   </Layout>
 )
 
