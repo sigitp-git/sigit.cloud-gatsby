09e3d95574e38290a68bc1cb66f15b77bac090a4
sigitp-git <sigitp@amazon.com>
2020-10-01 18:01:07 -0500

    title update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'sigit.cloud',
     author: 'Sigit Priyanggoro',
-    work: 'Sr Global Partner Solutions Architect',
+    work: 'Sr Partner Solutions Architect, 4G/5G Dedicated Edge',
     company: 'Amazon Web Services',
     city: 'Dallas',
     description:
