f3fb8d1ce5056829a2f849041f5efe8873501b26
Sigit Priyanggoro <sigitp@amazon.com>
2023-07-31 10:45:08 -0700

    patents

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -6,6 +6,7 @@ import SEO from "../components/seo"
 
 const Gallery = () => {
   const response = [
+    "4-patents-submitted.jpeg",
     "sigit-zk-mwc23-1.jpeg",
     "sigit-mwc23-ipw-demo-dbrown.jpeg",
     "_0 (6).JPG",
